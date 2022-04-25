861ad42414300d7c74b72744ae8b5cd4d55a8ed2
Sgt_Epic <adrian.koss@fh-bielefeld.de>
2022-04-25 14:50:00 +0200

    frontend init with vue.js

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    Hi
+  </header>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Welcome to Your Vue.js App</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView'
+}
+</script>
