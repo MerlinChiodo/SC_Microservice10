d474055c261e1b87f2d55fa76852604fcbb968c8
Sgt_Epic <adrian.koss@fh-bielefeld.de>
2022-04-30 11:54:40 +0200

    adds frontend html and css, dynamic data insertion is to be done

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <router-view/>
+  <header class="header">
+    <div class="container space-between">
+      <h1>Landingpage</h1>
+      <div class="login">
+        <button>Zum Login</button>
+      </div>
+    </div>
+  </header>
+  <div>
+    <ul class="container space-around">
+      <service-component />
+      <service-component />
+      <service-component />
+      <service-component />
+      <service-component />
+    </ul>
+    <ul class="container space-around">
+      <service-component />
+      <service-component />
+      <service-component />
+      <service-component />
+      <service-component />
+    </ul>
+  </div>
 </template>
 
 <style lang="scss">
@@ -7,20 +30,52 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.login {
+  position: relative;
+  top: 50%;
+  transform: translateY(25%);
 }
+
+button {
+  padding: 10px;
+  margin-right: 0;
+}
+
+body {
+  margin: 0;
+  background-color: #2c3e50;
+}
+
+.header {
+  background-color: #05ff97;
+  color: inherit;
+  margin-top: 0;
+  margin-bottom: 50px;
+  padding: 10px 0 10px 30px;
+}
+
+.container {
+  display: flex;
+  margin-right: 30px;
+}
+
+.container.space-around {
+  justify-content: space-around;
+}
+
+.container.space-between {
+  justify-content: space-between;
+}
+
 </style>
+<script>
+import ServiceComponent from '@/components/service'
+
+export default {
+  components: { ServiceComponent }
+}
+</script>
---
+++ frontend/src/components/service.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex-item">
+    <h2 class="title">Service 1</h2>
+    <p class="description">
+      turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas integer eget aliquet nibh praesent tristique magna sit amet purus gravida quis blandit turpis cursus in hac habitasse platea dictumst quisque sagittis purus sit amet volutpat consequat mauris nunc congue nisi vitae suscipit tellus mauris a diam maecenas sed enim ut sem viverra aliquet eget sit amet tellus cras adipiscing enim eu turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus mauris ultrices eros in cursus turpis massa tincidunt dui ut ornare lectus sit amet est placerat in egestas erat imperdiet sed euismod nisi porta lorem mollis aliquam ut porttitor leo a diam sollicitudin tempor id eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet risus nullam eget felis eget nunc lobortis mattis aliquam faucibus purus in massa tempor nec feugiat nisl pretium fusce id velit ut tortor pretium viverra suspendisse potenti nullam ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper
+    </p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'service-component'
+}
+</script>
+
+<style scoped>
+
+.title {
+  background-color: #05ff97;
+  color:inherit;
+  padding: 10px;
+}
+
+.description {
+  background-color: white;
+  padding: 10px;
+}
+
+.flex-item {
+  height: 300px;
+  width: 250px;
+  /*background-color: #05ff97;*/
+  margin: 0 10px 20px 0;
+  overflow: scroll;
+}
+
+</style>
